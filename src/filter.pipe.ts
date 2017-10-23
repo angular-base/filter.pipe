ef15baa71bae0ecb4931361e9f3a1b90adda4401
Sebastian Kalicki <kalicki2k@users.noreply.github.com>
2017-10-23 04:19:31 +0200

    Improve filter.pipe.ts

--- src/filter.pipe.ts
+++ src/filter.pipe.ts
@@ -1,14 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { isUndefined } from 'util';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform (items: any, query?: string): any {
+  transform(items: any[], query?: string): any[] {
 
-    if (isUndefined(query)) {
+    if (!query.length) {
       return items;
     }
 
@@ -16,5 +15,4 @@ export class FilterPipe implements PipeTransform {
       .filter(key => item[key] !== null)
       .some(key => (item[key].toString().toLowerCase()).indexOf(query.toLowerCase()) !== -1));
   }
-
 }
